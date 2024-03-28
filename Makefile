ea1e0312f46d91ea9c2d34a97388b0d9b450629c
Bastien Iannone <bastien.iannone@gmail.com>
2024-03-28 22:08:54 +0100

    first

---
+++ Makefile
@@ -0,0 +1,29 @@
+NAME = push_swap
+
+C_FILES = main.c
+SRCS = $(addprefix srcs/,$(C_FILES))
+
+CFLAGS = -Wall -Wextra -Werror
+
+LIBS = printf/libftprintf.a libft/libft.a
+DEPS = $(addprefix libs/,$(LIBS))
+
+all: $(NAME)
+
+$(NAME):
+	make -C libs/printf
+	make -C libs/libft
+	cc $(SRCS) -g3 -I includes -lm -lz -o $(NAME) $(DEPS)
+
+clean:
+	make -C libs/printf clean
+	make -C libs/libft clean
+
+fclean: clean
+	make -C libs/printf fclean
+	make -C libs/libft fclean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY:	all clean fclean re
---
+++ includes/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baiannon <baiannon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 20:39:04 by baiannon          #+#    #+#             */
+/*   Updated: 2024/03/28 20:39:05 by baiannon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "ft_printf.h"
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define INT_MIN -2147483648
+# define INT_MAX 2147483647
+
+#endif
